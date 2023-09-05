5d88f9557cd35405111bd945ad5df3ce6fa9edb7
RicardoZM <ricardo_zorita@hotmai.com>
2023-09-05 23:16:22 +0200

    Añadidos estilos a los elementos que componen cada carta

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,11 +2,17 @@ export const PokemonCard = (props) => {
   const { pokemon } = props;
   return (
     <div className="rounded-md shadow-lg">
-      <div className="flex items-center">
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <div className="relative items-center">
+        <img
+          className=""
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+        />
 
-        <h3 className="uppercase">{pokemon.name}</h3>
-        <div>#{pokemon.id}</div>
+        <h3 className="uppercase absolute inset-y-0 right-5 top-10 ">
+          {pokemon.name}
+        </h3>
+        <div className="absolute top-0 right-3">#{pokemon.id}</div>
       </div>
     </div>
   );
