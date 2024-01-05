0f2f0f83ee378462412abd168b121a99f10818a5
RicardoZM <ricardo_zorita@hotmai.com>
2024-01-05 01:49:50 +0100

    Create logos components

---
+++ src/icons/SearchLogo.jsx
@@ -0,0 +1,14 @@
+export const SearchLogo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M3 10a7 7 0 1 0 14 0 7 7 0 1 0-14 0M21 21l-6-6" />
+    </svg>
+  );
+};
